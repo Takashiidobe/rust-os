03c84dc489694dfb097f428a3443f932d871555f
Takashi Idobe <mail@takashiidobe.com>
2022-12-25 09:54:16 -0500

    10. Heap Allocation

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,18 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 #![feature(abi_x86_interrupt)]
+#![feature(alloc_error_handler)]
+
+#[alloc_error_handler]
+fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
+    panic!("allocation error: {:?}", layout)
+}
 
 use core::panic::PanicInfo;
 
+extern crate alloc;
+
+pub mod allocator;
 pub mod gdt;
 pub mod interrupts;
 pub mod memory;
